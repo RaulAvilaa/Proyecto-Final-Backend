66449f7b3f42be8702492627af80f62b1f67aabb
Raúl Avila <raul.avila.9604@gmail.com>
2024-08-02 11:59:02 -0300

    Entrega del proyecto finalizado (backend-coderhouse)

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Footer = () => {
     return <footer className="footer">
-        <p>Rodrigo Vergara 2023 - Portafolio <a href="http://rodrigovergara.com.ar">aqui</a></p>
     </footer>
 }
 
